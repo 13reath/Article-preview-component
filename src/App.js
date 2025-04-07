7af19c0ff4bb2dea255bf87a2948e27965dbb141
13reathcode <new13reath@gmail.com>
2025-04-07 23:23:39 +0300

    mobile friendly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [active, setActive] = useState(true);
+
+  function handleClick() {
+    setActive((active) => !active);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="card">
+        <div className="card-image">
+          <img
+            className="banner"
+            src={process.env.PUBLIC_URL + "/images/drawers.jpg"}
+            alt="img"
+          />
+        </div>
+
+        <div className="card-content">
+          <h1 className="card-heading">
+            Shift the overall look and feel by adding these wonderful touches to
+            furniture in your home
+          </h1>
+          <p className="card-description">
+            Ever been in a room and felt like something was missing? Perhaps it
+            felt slightly bare and uninviting. I've got some simple tips to help
+            you make any room feel complete.
+          </p>
+        </div>
+        {active ? (
+          <div className="profile">
+            <img
+              className="profile-image"
+              src={process.env.PUBLIC_URL + "./images/avatar-michelle.jpg"}
+              alt="avatar"
+            />
+            <div className="profile-info">
+              <p>Michelle Appleton</p>
+              <span>28 Jun 2020</span>
+            </div>
+            <button onClick={handleClick} class="profile-icon-share">
+              <img
+                src={process.env.PUBLIC_URL + "./images/icon-share.svg"}
+                alt="share"
+                className="share-icon"
+              />
+            </button>
+          </div>
+        ) : (
+          <div className="profile-alt">
+            <p>SHARE</p>
+
+            <div className="profile-icons">
+              <img
+                src={process.env.PUBLIC_URL + "./images/icon-facebook.svg"}
+                alt="facebook"
+              />
+              <img
+                src={process.env.PUBLIC_URL + "./images/icon-pinterest.svg"}
+                alt="pinterest"
+              />
+              <img
+                src={process.env.PUBLIC_URL + "./images/icon-twitter.svg"}
+                alt="twitter"
+              />
+            </div>
+
+            <button onClick={handleClick} className="profile-icon-share">
+              <img
+                src={process.env.PUBLIC_URL + "./images/icon-share.svg"}
+                alt="share"
+                className="share-icon"
+              />
+            </button>
+          </div>
+        )}
+      </div>
+    </main>
   );
 }
-
-export default App;
